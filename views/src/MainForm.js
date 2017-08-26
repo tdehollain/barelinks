bfd3080aaf813b99f036f2401da411684210afd9
Thibaut de Hollain <tdehollain@Thibauts-MacBook-Pro.local>
2017-08-27 00:16:57 +0200

    commit

--- views/src/MainForm.js
+++ views/src/MainForm.js
@@ -2,33 +2,19 @@ import React, { Component } from 'react';
 
 class MainForm extends Component {
 
-	constructor(props) {
-		super(props);
-
-		this.focus = this.focus.bind(this);
-	}
-
-	focus() {
-		this.textInput.focus();
-	}
-
 	submitLink(e) {
 		alert();
 		e.preventDefault();
-	}
-
-	componentDidMount() {
-		this.focus();
 	}
 
 	render() {
 		return (
 			<div className='MainForm'>
 				<form onSubmit={e => this.props.handleSubmit(e)}>
-					<input 
+					<input
+						id='urlInput'
 						type='url' 
 						placeholder='enter URL'
-						ref={input => { this.textInput = input }}
 						value={this.props.URLvalue}
 						onChange={this.props.handleURLchange}
 					/>
--- views/src/index.js
+++ views/src/index.js
@@ -6,3 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+window.onload = function (){
+	document.getElementById('urlInput').focus();
+};
