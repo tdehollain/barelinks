b55801aa6d1f7848427acc5d539b7d54cb506318
Thibaut de Hollain <tdehollain@Thibauts-MBP.lan>
2018-03-22 14:28:54 +0100

    changed max nb of tags in modal from 5 to 15

--- views/src/store.js
+++ views/src/store.js
@@ -10,7 +10,7 @@ const initialUserState = {
 		maxTags: 3,
 		linksPerPage: 50,
 		maxTagsToShow: 200,
-		maxTagsToShowInModal: 5
+		maxTagsToShowInModal: 15
 	},
 	tagColors: ['B0BEC5','BCAAA4','FFCC80','FFF59D','C5E1A5','80CBC4','81D4FA','9FA8DA','CE93D8','EF9A9A']
 };
