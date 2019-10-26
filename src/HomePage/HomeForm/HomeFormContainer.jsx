a5029e71a80fb59ca7d2b823199f81a348c50bc4
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2019-10-26 16:44:32 +0200

    added hooks

--- src/HomePage/HomeForm/HomeFormContainer.jsx
+++ src/HomePage/HomeForm/HomeFormContainer.jsx
@@ -22,38 +22,6 @@ const HomeFormContainer = ({ username, addLink }) => {
   return <HomeForm handleChange={handleChange} enteredURL={enteredURL} handleSubmit={handleSubmit} />;
 };
 
-// class HomeFormContainer extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       enteredURL: ''
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({
-//       enteredURL: e.target.value
-//     });
-//   }
-
-//   async handleSubmit(e) {
-//     e.preventDefault();
-//     let url = this.state.enteredURL;
-//     if (URLisValid(url)) {
-//       await this.props.addLink(this.props.username, url);
-//       this.setState({ enteredURL: '' });
-//     }
-//   }
-
-//   render() {
-//     return <HomeForm handleChange={this.handleChange} enteredURL={this.state.enteredURL} handleSubmit={this.handleSubmit} />;
-//   }
-// }
-
 const mapStateToProps = store => {
   return {
     username: store.userReducer.username
