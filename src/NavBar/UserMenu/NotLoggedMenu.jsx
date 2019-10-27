b89d060c5d4fb7a0a269c66bec5a8518cf77fa50
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2019-10-27 23:23:01 +0100

    fixed buttons location

--- src/NavBar/UserMenu/NotLoggedMenu.jsx
+++ src/NavBar/UserMenu/NotLoggedMenu.jsx
@@ -5,37 +5,29 @@ import GoSignIn from 'react-icons/lib/go/sign-in';
 import { Link } from 'react-router-dom';
 
 class NotLoggedMenu extends Component {
-	render() {
-		return (
-			<div className='userMenu'>
-				<div className='userButton'>
-					<button className='btn btn-outline-secondary mr-3'>
-						<IoPerson style={{ fontSize: '1.2rem' }} className='mr-2' />
-						Log in / Sign up
-					</button>
-				</div>
-				<div className='userMenuDropDown'>
-					<div className='loginButton'>
-						<Link to='/login'>
-							<button className='btn btn-outline-secondary mr-3'>
-								<GoSignIn className='mr-2' />
-								Log in
-							</button>
-						</Link>
-					</div>
-					<hr style={{ background: '#ECEFF1', margin: '0 8px' }}></hr>
-					<div className='signUpButton'>
-						<Link to='/signup'>
-							<button className='btn btn-outline-secondary mr-3'>
-								<IoPersonAdd style={{ fontSize: '1.2rem' }} className='mr-2' />
-								Sign up
-							</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="form-inline userMenu">
+        <div className="loginButton">
+          <Link to="/login">
+            <button className="btn btn-outline-secondary mr-3">
+              <GoSignIn className="mr-2" />
+              Log in
+            </button>
+          </Link>
+        </div>
+        <hr style={{ background: '#ECEFF1', margin: '0 8px' }}></hr>
+        <div className="signUpButton">
+          <Link to="/signup">
+            <button className="btn btn-outline-secondary mr-3">
+              <IoPersonAdd style={{ fontSize: '1.2rem' }} className="mr-2" />
+              Sign up
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotLoggedMenu;
