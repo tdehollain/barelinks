60d587041674f1427f9a42c0cd5e15a1262fe34c
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2025-10-27 23:48:33 +0100

    cleanup

--- db/migrations/001_update_links_table.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Update links table structure
--- Remove tags TEXT[] column from links table
-
-BEGIN;
-
--- Remove the tags column from links table if it exists
-ALTER TABLE links DROP COLUMN IF EXISTS tags;
-
-COMMIT;
